e4dc0158f57f6f60231e1b0e15e61af9874e0641
진민 <insomnis2020@naver.com>
2024-05-16 00:27:13 +0900

    AGGrid 적용

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -8,6 +8,7 @@ import PublicLayout from '../layouts/public.vue'
 import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
 import PostList from '../views/PostList.vue'
+import Reservation from '../views/Reservation.vue'
 
 
 const routes = [
@@ -33,6 +34,18 @@ const routes = [
     path: "/notification",
     name: "Post",
     component: PostList,
+    meta : {layout: PublicLayout}
+  },
+  {
+    path: "/qna",
+    name: "Post",
+    component: PostList,
+    meta : {layout: PublicLayout}
+  },
+  {
+    path: "/reservation",
+    name: "Post",
+    component: Reservation,
     meta : {layout: PublicLayout}
   },
 ];
